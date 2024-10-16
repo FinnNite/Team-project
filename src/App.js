5792f8b18a82cad72ee2fdf67913a1f55d1c761f
FinnNite <seawolf133700@gmail.com>
2024-10-16 12:03:25 +0400

    Добавлен перенос элемента в рабочую область

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './css/App.css';
 import './css/shapes.css';
-import { useEffect, useState, useRef } from 'react';
-import { draggable, dropTargetForElements } from '@atlaskit/pragmatic-drag-and-drop/element/adapter';
-import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
-import { attachClosestEdge, extractClosestEdge } from "@atlaskit/pragmatic-drag-and-drop-hitbox/closest-edge"; 
+import { Rectangle, Oval, Arrow } from './shapes.js';
+import { useState } from 'react';
 import lupa from "./image/lupa.png";
 import table from "./image/table.png";
 import copy from "./image/copy.png";
-import invariant from 'tiny-invariant';
 
 function App() {
   return (
@@ -16,7 +13,7 @@ function App() {
       <SearchContainer />
       <ToolbarContainer />
       <RightbarContainer />
-      <WorkspaceContainer/>
+      <WorkspaceContainer columnId={1} title={"WSContainer"} />
     </>
   );
 }
@@ -48,15 +45,24 @@ function SearchContainer() {
   );
 }
 
-function ToolbarContainer() {
+const ToolbarContainer = ({onDrop}) => {
+  const itemsClass1 = [
+  { component: <Rectangle/>, type: 'rectangle' },
+  { component: <Oval/>, type: 'oval'}, 
+  { component: <Arrow/>, type: 'arrow'}]
   return (
     <div className="toolbar container">
 			<nav className="tools">
 				<h3>Класс 1</h3>
 				<ul className="tools">
-					<Rectangle />
-					<Oval />
-					<li>Стрелка</li>
+					{itemsClass1.map((item, index) => (
+            <div key={index} 
+            draggable 
+            onDragStart={(e) => e.dataTransfer.setData('text/plain', item.type)}
+            >
+              {item.component}
+              </div>
+          ))}
 				</ul>
 				<h3>Класс 2</h3>
 				<ul className="tools">
@@ -83,126 +89,36 @@ function RightbarContainer() {
   );
 }
 
-function WorkspaceContainer({columnId, title, rects}) {
-  const spaceRef = useRef(null);
-  const [isDraggedOver, setIsDraggedOver] = useState(false);
+function WorkspaceContainer() {
+  const [shapes, setShapes] = useState([]);
 
-  useEffect(() => {
-    const spaceE1 = spaceRef.current;
-    invariant(spaceE1);
+  const handleDrop = (e) => {
+    e.preventDefault();
+    const shape = e.dataTransfer.getData('text/plain');
+    if (shape) {
+      setShapes((prevShapes) => [...prevShapes, shape])
+    }
+  }
 
-    return dropTargetForElements({
-        element: spaceE1,
-        onDragStart: () => setIsDraggedOver(true),
-        onDragEnter: () => setIsDraggedOver(true),
-        onDragLeave: () => setIsDraggedOver(false),
-        onDrop: () => setIsDraggedOver(false),
-        getData: () => ({ columnId }),
-    });
-  }, [columnId]);
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div ref={spaceRef} 
-    className={`workspace container ${isDraggedOver ? "dragged-over" : ""}`}>
+    <div onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      className={"workspace container"}
+    >
+      <h3>Canvas</h3>
+      {shapes.map((shape, index) => (
+        <div key={index}>
+          {shape === 'rectangle' ? <Rectangle/> : (shape === 'oval' ? <Oval/> : (shape === 'arrow' ? <Arrow/> : null))}
+        </div>
+      ))}
     </div>
   );
 }
 
-const Rectangle = (children, ...rect) => {
-  const ref = useRef(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const [closestEdge, setClosestEdge] = useState(null); 
 
-  useEffect(() => {
-    const rectE1 = ref.current;
-    invariant(rectE1);
-
-    return combine(
-      draggable({
-      element: rectE1,
-      getInitialData: () => ({ type: "rectangle", rectId: rect.id }), 
-      onDragStart: () => setIsDragging(true),
-      onDrop: () => setIsDragging(false),
-    }),
-    dropTargetForElements({
-      element: rectE1,
-      getData: ({ input, element }) => {
-        const data = { type: "rectangle", rectId: rect.id};
-        return attachClosestEdge(data, {
-          input,
-          element,
-          allowedEdges: ["top", "bottom"],
-        });
-      },
-      getIsSticky: () => true,
-      onDragEnter: (args) => {
-        if (args.source.data.rectId != rect.id) {
-          setClosestEdge(extractClosestEdge(args.self.data));
-        }
-      },
-      onDrag: (args) => {
-        if (args.source.data.rectId != rect.id) {
-          setClosestEdge(args.self.data);
-        }
-      },
-      onDragLeave: () => {
-        setClosestEdge(null);
-      },
-      onDrop: () => {
-        setClosestEdge(null);
-      },
-    })
-  );
-  }, [rect.id]);
-
-  return (
-    <div
-      className={`rectangle ${isDragging ? "dragging" : ""}`} 
-      ref={ref}>
-    </div>
-  );
-}
-
-const Oval = (children, ...oval) => {
-  const ref = useRef(null);
-  const [isDragging, setIsDragging] = useState(false);
-
-  useEffect(() => {
-    const ovalE1 = ref.current;
-    invariant(ovalE1);
-
-    return combine(
-      draggable({
-      element: ovalE1,
-      getInitialData: () => ({ type: "oval", ovalId: oval.id }), 
-      onDragStart: () => setIsDragging(true),
-      onDrop: () => setIsDragging(false),
-    }),
-    dropTargetForElements({
-      element: ovalE1,
-      getData: ({ input, element }) => {
-        const data = { type: "oval", ovalId: oval.id};
-
-        return attachClosestEdge(data, {
-          input,
-          element,
-          allowedEdges: ["top", "bottom"],
-        });
-      },
-      getIsSticky: () => true,
-      onDragEnter: (args) => {
-        if (args.source.data.ovalId != oval.id) {
-          console.log("onDragEvent", args);
-        }
-      }
-    })
-  );
-  }, [oval.id]);
-
-  return (
-    <div
-      ref={ref}
-      className={`oval ${isDragging ? "dragging" : ""}`}></div>
-  )
-}
 
 export default App;
